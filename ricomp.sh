ac6e19817ec9a5c2d62b94b57ddbb51723208aa8
yangboz <youngwelle@gmail.com>
2018-01-30 16:02:26 +0800

    CHANGED:modified the ricomp.sh startup script;

--- ricomp.sh
+++ ricomp.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ##Mediawiki,Wordpress,Piwik,Phabricator,OpenModelica,Jenkins
-cd ~/TensorCloud & git pull
-docker-compose -f mariadb_mediawiki.yml -f wordpress.yml -f piwik.yml -f phabricator.yml -f openmodelica-webbook.yml -f jenkins.yml -d
+#cd ~/TensorCloud
+docker-compose -f /home/toyhouse/TensorCloud/mariadb_mediawiki.yml -f /home/toyhouse/TensorCloud/wordpress.yml -f /home/toyhouse/TensorCloud/piwik.yml -f /home/toyhouse/TensorCloud/phabricator.yml -f /home/toyhouse/TensorCloud/openmodelica-webbook.yml -f /home/toyhouse/TensorCloud/jenkins.yml up -d
 ##ResourceSpace
-~/resourcespace-8.3.1/ctlscript.sh start
+/home/toyhouse/resourcespace-8.3-1/ctlscript.sh start
