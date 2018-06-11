50c87795aecf115af9eb59902ca3282de016cfe4
yangboz <youngwelle@gmail.com>
2018-06-11 13:20:16 +0800

    ADDED:start up script for mediawiki/wordpress;

---
+++ DockerInDocker/mariadb_mediawiki_4444.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+##Mediawiki
+mkdir -p TensorCloud/DockerInDocker/mariadb_data
+mkdir -p TensorCloud/DockerInDocker/mediawiki_data
+chmod -R 777 TensorCloud/DockerInDocker/mariadb_data
+chmod -R 777 TensorCloud/DockerInDocker/mediawiki_data
+#cd ~/TensorCloud
+docker-compose -f TensorCloud/DockerInDocker/mariadb_mediawiki.yml up -d
+#port forwarding
+iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 4444 -j REDIRECT --to-port 80
---
+++ DockerInDocker/mariadb_wordpress_4445.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+##Mediawiki
+mkdir -p TensorCloud/DockerInDocker/mariadb_data
+mkdir -p TensorCloud/DockerInDocker/wordpress_data
+chmod -R 777 TensorCloud/DockerInDocker/mariadb_data
+chmod -R 777 TensorCloud/DockerInDocker/wordpress_data
+#cd ~/TensorCloud
+docker-compose -f TensorCloud/DockerInDocker/mariadb_wordpress.yml up -d
+#port forwarding
+iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 4445 -j REDIRECT --to-port 80
