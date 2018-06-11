c609c85394260ea07fc608083b6494eaa9c5a65c
yangboz <youngwelle@gmail.com>
2018-06-11 13:30:13 +0800

    CHANGED:updated the start up script;

--- DockerInDocker/mariadb_mediawiki_4444.sh
+++ DockerInDocker/mariadb_mediawiki_4444.sh
@@ -4,7 +4,7 @@ mkdir -p TensorCloud/DockerInDocker/mariadb_data
 mkdir -p TensorCloud/DockerInDocker/mediawiki_data
 chmod -R 777 TensorCloud/DockerInDocker/mariadb_data
 chmod -R 777 TensorCloud/DockerInDocker/mediawiki_data
-#cd ~/TensorCloud
-docker-compose -f TensorCloud/DockerInDocker/mariadb_mediawiki.yml up -d
+cd ~/TensorCloud/DockerInDocker
+docker-compose -f mariadb_mediawiki.yml up -d
 #port forwarding
 iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 4444 -j REDIRECT --to-port 80
