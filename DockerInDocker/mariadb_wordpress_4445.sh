03cb781d40b67ccb834ea04aed3353c7bc025f93
yangboz <youngwelle@gmail.com>
2018-06-11 13:38:32 +0800

    CHANGED:updated the start up script;

--- DockerInDocker/mariadb_wordpress_4445.sh
+++ DockerInDocker/mariadb_wordpress_4445.sh
@@ -4,7 +4,7 @@ mkdir -p TensorCloud/DockerInDocker/mariadb_data
 mkdir -p TensorCloud/DockerInDocker/wordpress_data
 chmod -R 777 TensorCloud/DockerInDocker/mariadb_data
 chmod -R 777 TensorCloud/DockerInDocker/wordpress_data
-cd ~/TensorCloud/DockerInDocker
+cd /TensorCloud/DockerInDocker
 docker-compose -f mariadb_wordpress.yml up -d
 #port forwarding
 iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 4445 -j REDIRECT --to-port 80
