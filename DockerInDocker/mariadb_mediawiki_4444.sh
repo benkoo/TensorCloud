62e601f3a4ad1ec208b97329d4f7f017d88de8e3
yangboz <youngwelle@gmail.com>
2018-06-11 16:24:16 +0800

    ADDED:start up script for mediawiki/wordpress;

--- DockerInDocker/mariadb_mediawiki_4444.sh
+++ DockerInDocker/mariadb_mediawiki_4444.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 ##Mediawiki
-mkdir -p TensorCloud/DockerInDocker/mariadb_data
-mkdir -p TensorCloud/DockerInDocker/mediawiki_data
-chmod -R 777 TensorCloud/DockerInDocker/mariadb_data
-chmod -R 777 TensorCloud/DockerInDocker/mediawiki_data
 cd /TensorCloud/DockerInDocker
+mkdir -p mariadb_data
+mkdir -p mediawiki_data
+chmod -R 777 mariadb_data
+chmod -R 777 mediawiki_data
 docker-compose -f mariadb_mediawiki.yml up -d
 #port forwarding
 iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 4444 -j REDIRECT --to-port 80
+
+exit 0
--- DockerInDocker/mariadb_wordpress_4445.sh
+++ DockerInDocker/mariadb_wordpress_4445.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 ##Mediawiki
-mkdir -p TensorCloud/DockerInDocker/mariadb_data
-mkdir -p TensorCloud/DockerInDocker/wordpress_data
-chmod -R 777 TensorCloud/DockerInDocker/mariadb_data
-chmod -R 777 TensorCloud/DockerInDocker/wordpress_data
 cd /TensorCloud/DockerInDocker
+mkdir mariadb_data
+mkdir wordpress_data
+chmod -R 777 mariadb_data
+chmod -R 777 wordpress_data
+
 docker-compose -f mariadb_wordpress.yml up -d
 #port forwarding
 iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 4445 -j REDIRECT --to-port 80
+
+exit 0
