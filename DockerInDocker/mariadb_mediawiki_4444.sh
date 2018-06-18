37a42e555b2e2b356393970db2777e86664401eb
yangboz <youngwelle@gmail.com>
2018-06-18 10:15:40 +0800

    CHANGED:preload dind required mariadb/mediawiki images in DinD

--- DockerInDocker/mariadb_mediawiki_4444.sh
+++ DockerInDocker/mariadb_mediawiki_4444.sh
@@ -5,6 +5,11 @@ mkdir -p mariadb_data
 mkdir -p mediawiki_data
 chmod -R 777 mariadb_data
 chmod -R 777 mediawiki_data
+#load docker images
+docker load < saved_bitnami_mariadb_latest.tar.gz 
+sleep 5
+docker load < saved_smartkit_mediawiki_locale.tar.gz
+sleep 5
 docker-compose -f mariadb_mediawiki.yml up -d
 #port forwarding
 iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 4444 -j REDIRECT --to-port 80
