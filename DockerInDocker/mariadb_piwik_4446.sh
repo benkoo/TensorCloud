efbc98b104c86079f1481f7909a797268377e55a
yangboz <youngwelle@gmail.com>
2018-06-19 09:31:21 +0800

    CHANGED:preloading mariadb_piwik_latest images for Dind;

--- DockerInDocker/mariadb_piwik_4446.sh
+++ DockerInDocker/mariadb_piwik_4446.sh
@@ -5,7 +5,11 @@ mkdir -p mariadb_data
 mkdir -p piwik_data
 chmod -R 777 mariadb_data
 chmod -R 777 piwik_data
-
+#load docker images
+docker load < saved_bitnami_mariadb_latest.tar.gz 
+sleep 5
+docker load < saved_bitnami_piwik_latest.tar.gz
+sleep 5
 docker-compose -f mariadb_piwik.yml up -d
 #port forwarding
 iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 4446 -j REDIRECT --to-port 80
