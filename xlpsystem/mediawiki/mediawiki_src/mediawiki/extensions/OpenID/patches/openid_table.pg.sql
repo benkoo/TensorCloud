27a82c16df44c5fa9212e9c2c314932ebada75cc
Yepeng Tian <yepeng.tian@dookay.com>
2018-08-13 18:23:00 +0800

    rename to mediawiki_src and add CMD

---
+++ xlpsystem/mediawiki/mediawiki_src/mediawiki/extensions/OpenID/patches/openid_table.pg.sql
@@ -0,0 +1,10 @@
+
+-- Schema for the OpenID extension (Postgres version)
+
+CREATE TABLE /*_*/user_openid (
+  uoi_openid VARCHAR(255) NOT NULL PRIMARY KEY,
+  uoi_user   INTEGER NOT NULL REFERENCES mwuser(user_id),
+  uoi_user_registration TIMESTAMPTZ
+);
+
+CREATE INDEX /*i*/user_openid_user ON /*_*/user_openid(uoi_user);
