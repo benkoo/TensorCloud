91592fff6cab2811cb83a1439aa44849f7e0d737
Yepeng Tian <yepeng.tian@dookay.com>
2018-08-16 15:57:39 +0800

    Stable version 1

--- xlpsystem/wordpress/xlp_init.sh
+++ xlpsystem/wordpress/xlp_init.sh
@@ -9,8 +9,3 @@ mv /tmp/xlp_dev /
 # link xlp_dev/wordpress wordpres_src
 rm -rf /usr/src/wordpress
 ln -s /xlp_dev/wordpress /usr/src/wordpress
-cp -R /xlp_dev/wordpress /var/www/html/wordpress
-
-# link xlp_data/uploads
-# rm -rf /var/www/html/wp-content/uploads
-# ln -s /xlp_data/uploads /var/www/html/wp-content/uploads
--- xlpsystem/wordpress/xlp_start.sh
+++ xlpsystem/wordpress/xlp_start.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
 /usr/local/bin/docker-entrypoint.sh apache2-foreground
-
