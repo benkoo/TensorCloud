3d5940db357bff284f89c781454bb5ba2b703198
Yepeng Tian <yepeng.tian@dookay.com>
2018-08-23 11:26:28 +0800

    Change piwik.js form xlp_dev to xlp_data

--- xlpsystem/mediawiki/xlp_data/piwik.js
+++ xlpsystem/mediawiki/xlp_data/piwik.js
@@ -3,7 +3,7 @@ var _paq = _paq || [];
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function() {
-  var u="//hotbackup.toyhouse.cc:82/";
+  var u="//main.toyhouse.cc:82/";
   _paq.push(['setTrackerUrl', u+'piwik.php']);
   _paq.push(['setSiteId', '1']);
   var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
