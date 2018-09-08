c687b65ea7b6032ec7ad25303723d39c13476ac1
joshuaguan <garfilone@gmail.com>
2018-09-08 13:29:05 +0800

    add phabricator into xlp system

--- xlpsystem/phabricator /preflight.sh
+++ xlpsystem/phabricator /preflight.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-if [ ! -d /phabricator/ ]; then
-    mkdir -p /phabricator/data/
-    mkdir -p /phabricator/config/mysql
-    cp my.cnf /phabricator/config/mysql/
+if [ ! -d /data/xlpsystem/phabricator/ ]; then
+    sudo mkdir -p /data/xlpsystem/phabricator/data/mysql
+    sudo mkdir -p /data/xlpsystem/phabricator/config/mysql
+    sudo mkdir -p /data/xlpsystem/phabricator/repos
+    sudo mkdir -p /data/xlpsystem/phabricator/extensions
+    sudo cp my.cnf /data/xlpsystem/phabricator/config/mysql
 fi
