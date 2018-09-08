87f76ce4de9bdf39b4f6f3a6f80dd75e498d64aa
joshuaguan <garfilone@gmail.com>
2018-09-08 13:34:58 +0800

    cleanup

--- xlpsystem/phabricator /preflight.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if [ ! -d /data/xlpsystem/phabricator/ ]; then
-    sudo mkdir -p /data/xlpsystem/phabricator/data/mysql
-    sudo mkdir -p /data/xlpsystem/phabricator/config/mysql
-    sudo mkdir -p /data/xlpsystem/phabricator/repos
-    sudo mkdir -p /data/xlpsystem/phabricator/extensions
-    sudo cp my.cnf /data/xlpsystem/phabricator/config/mysql
-fi
